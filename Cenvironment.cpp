6351b45b679e16da382bfb980ec2e082d7c400e9
Dangomimikyu <54048830+Dangomimikyu@users.noreply.github.com>
2020-02-21 04:54:28 +0800

    Update Cenvironment.cpp

--- Cenvironment.cpp
+++ Cenvironment.cpp
@@ -1,79 +1,31 @@
-#include "Cenvironment.h"
-
-Cenvironment::Cenvironment() : collided(false), distance(0), radius(0), overlap(0)
+#ifndef CENV_H
+#define CENV_H
+#include "Vertex.h"
+#include <Vector3.h>
+#include <transformation.h>
+class Cenvironment
 {
+private:
+	bool collided;
+	float distance;
+	float radius;
+	float overlap;
+	Position position;
+	transform transformations;
+	static Cenvironment* myObject;
 
-}
+public:
+	Cenvironment();
+	~Cenvironment();
+	static Cenvironment* get_instance();
 
-Cenvironment::~Cenvironment()
-{
-}
+	bool getcollide() const;
+	float getDistance() const;
+	float getoverlap() const;
+	transform get_transformation(void) const;
 
-Cenvironment* Cenvironment::get_instance()
-{
-	if (!myObject) {
-		myObject = new Cenvironment();
-		return myObject;
-	}
-	else {
-		return myObject;
-	}
-}
-Cenvironment* Cenvironment::myObject = 0;
-void Cenvironment::roundCollision(Cenvironment& object, float r)
-{
-	radius = r;
-	/// pythagoras' theorem to find the length
-	//distance = sqrt(pow(this->get_transformation().translation.x - object.position.x, 2) + pow(this->get_transformation().translation.z - object.position.z, 2));
-	distance = sqrt(pow(this->get_transformation().translation.x - get_instance()->get_transformation().translation.x, 2) + pow(this->get_transformation().translation.z - get_instance()->get_transformation().translation.z, 2));
-	if (distance >= r + get_instance()->radius)
-	{
-		collided = false;
-	}
-	else if (distance < r + get_instance()->radius)
-	{
-		collided = true;
-		//overlap = /*0.5 **/ (distance - p.radius - r);
-		overlap = (get_instance()->radius + r) - distance;
-	}
-}
-
-bool Cenvironment::getcollide() const
-{
-	return collided;
-}
-
-float Cenvironment::getDistance() const
-{
-	return distance;
-}
-
-float Cenvironment::getoverlap() const
-{
-	return overlap;
-}
-
-void Cenvironment::set_transformation(float angle, Vector3 vector)
-{
-	transformations.rotateAngle = angle;
-	transformations.rotation = vector;
-}
-
-void Cenvironment::set_transformation(char type, Vector3 vector)
-{
-	if (type == 't') { // translate
-		transformations.translation = vector;
-	}
-	else if (type == 's') { // scale
-		transformations.scaling = vector;
-	}
-	else {
-		throw ("you entered something that is not translate or scale");
-	}
-}
-
-transform Cenvironment::get_transformation(void) const
-{
-	return transformations;
-
-}
+	void set_transformation(float angle, Vector3 vector);//rotation
+	void set_transformation(char type, Vector3 vector);
+	virtual void roundCollision(Cenvironment& object, float radius);
+};
+#endif
