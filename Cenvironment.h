614007f29ceeb60e677d5794a38dce7c6db49706
Dangomimikyu <54048830+Dangomimikyu@users.noreply.github.com>
2020-02-20 15:31:57 +0800

    Update Cenvironment.h

--- Cenvironment.h
+++ Cenvironment.h
@@ -1,5 +1,5 @@
-#pragma once
-#include "player.h"
+#ifndef CENV_H
+#define CENV_H
 #include "Vertex.h"
 #include <Vector3.h>
 #include <transformation.h>
@@ -12,17 +12,20 @@ private:
 	float overlap;
 	Position position;
 	transform transformations;
+	static Cenvironment* myObject;
+
 public:
 	Cenvironment();
 	~Cenvironment();
+	static Cenvironment* get_instance();
+
 	bool getcollide() const;
 	float getDistance() const;
 	float getoverlap() const;
-	transform getTransformation(void) const;
+	transform get_transformation(void) const;
 
-	void setPosition(float, float);
-	void set_transformationR(float angle, Vector3 vector);//rotation
+	void set_transformation(float angle, Vector3 vector);//rotation
 	void set_transformation(char type, Vector3 vector);
-	virtual void roundCollision(player, float);
+	virtual void roundCollision(Cenvironment& object, float radius);
 };
-
+#endif
