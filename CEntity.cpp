81be1766da839c73aec16c8ef14ec2541dca10ac
Dangomimikyu <54048830+Dangomimikyu@users.noreply.github.com>
2020-02-22 14:41:43 +0800

    Update CEntity.cpp

--- CEntity.cpp
+++ CEntity.cpp
@@ -3,18 +3,3 @@
 CEntity::CEntity() : name("bob")
 {
 }
-#ifndef CENTITY_H
-#define CENTITY_H
-#include <string>
-#include <Mtx44.h>
-#include <transformation.h>
-
-class CEntity
-{
-private:
-	std::string name;
-public:
-	CEntity();
-};
-
-#endif // !CENTITY_H
