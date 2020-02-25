106c04b580b1e1ed552cfd569431a70eb1110cd2
Dangomimikyu <54048830+Dangomimikyu@users.noreply.github.com>
2020-02-25 10:36:06 +0800

    Update transformation.h

--- transformation.h
+++ transformation.h
@@ -8,6 +8,7 @@ struct Vector4
 	Vector4(float angle = 0, float x = 0, float y = 0, float z = 0) { Set(angle, x, y, z); }
 	void Set(float angle = 0, float x = 0, float y = 0, float z = 0) { this->angle = angle, this->x = x, this->y = y, this->z = z; }
 
+	
 };
 
 struct transform {
@@ -16,6 +17,12 @@ struct transform {
 	Vector4 rotationY = Vector4(0, 0, 1, 0);
 	Vector4 rotationZ = Vector4(0, 0, 0, 1);
 	Vector3 scaling = Vector3(1, 1, 1);
+
+	transform operator+(transform other) const;
+	transform operator-(transform other) const;
+	transform operator*(transform other) const;
+	transform operator/(transform other) const;
+
 };
 
 #endif // ! TRANSFORM_H
